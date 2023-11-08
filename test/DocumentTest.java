c0650a4402ca0a452c9b9c0d935a3bac572db08a
Mateusz <mswiatek2002@gmail.com>
2023-11-08 22:18:33 +0100

    Initial Commit

--- test/DocumentTest.java
+++ test/DocumentTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
